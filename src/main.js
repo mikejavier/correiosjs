59998f7fe8bf1aed15072fb338df1b69503a832d
Michael Santillán <contato@michaelsantillan.com>
2018-08-20 00:14:33 -0300

    Update main.js code

--- src/main.js
+++ src/main.js
@@ -2,22 +2,20 @@ import soap from 'soap';
 
 const BASE_URL = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?wsdl';
 
-function makeClient(cb) {
-  soap.createClient(BASE_URL, function (err, client) {
-    if (err) return console.log(err);
-    cb(client);
-  });
-}
-
-const getListServices = () => {
-  return new Promise(function(resolve, reject) {
-    makeClient(function (client) {        
-      client.ListaServicos(null, function (error, result) {
-        if (error) return reject(error);
-        resolve(result);
-      });
+function makeClient() {
+  return new Promise((resolve, reject) => {
+    soap.createClient(BASE_URL, (err, client) => {
+      if (err) return reject(err);
+      return resolve(client);
     });
   });
 }
+
+const getListServices = () => new Promise((resolve, reject) => {
+  makeClient().then(client => client.ListaServicos(null, (err, result) => {
+    if (err) reject(err);
+    resolve(result);
+  }));
+});
 
 export default getListServices;
